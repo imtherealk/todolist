be86c9f10d76e7456c83bdbc6cc5b2c099cb9961
imtherealk <imtherealk@gmail.com>
2015-06-04 11:48:21 +0900

    메인 액티비티(TodoList)의 리스트뷰  / addItemActivity 호출

---
+++ java/com/realk/todolist/addItemActivity.java
@@ -0,0 +1,12 @@
+package com.realk.todolist;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class addItemActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+    }
+}
